5e9598771cafe9431f4691a19adc39ccd04c9282
Shakil Hossain <sh9743441@gmail.com>
2021-08-23 19:01:12 +0600

    nested routes

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import queryString from 'query-string';
 
-const Posts = () => {
+const Posts = ({match, location}) => {
+  const results = queryString.parse(location.search);
+
+  console.log(results);
+
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year: {match.params.year} , Month: {match.params.month}
     </div>
   );
 };
